d7530d7e10ece256f41ef014b280ee9733b76016
Je Xia <i@jex.me>
2025-05-25 23:21:47 +0800

    Update lazy.ts

--- runtime/lazy.ts
+++ runtime/lazy.ts
@@ -7,29 +7,24 @@ customElements.define(
       // set a timeout to wait for the element to be fully parsed
       setTimeout(() => {
         if (!this.placeholder) {
-          this.placeholder = [...this.childNodes].filter(
-            (child) => {
-              if (child.nodeType === 1 && (child as Element).tagName === "TEMPLATE") {
-                if ((child as HTMLTemplateElement).hasAttribute("data-props")) {
-                  this.props = (child as HTMLTemplateElement).content.textContent!;
-                }
-                return false;
-              }
-              return true;
-            },
-          );
+          this.placeholder = [...this.childNodes].filter((child) => {
+            if (child.nodeType === 1 && (child as Element).tagName === "TEMPLATE" && (child as Element).hasAttribute("data-props")) {
+              this.props = (child as HTMLTemplateElement).content.textContent!;
+              return false;
+            }
+            return true;
+          });
         }
-        fetch(location.href, {
-          headers: {
-            "x-component": this.getAttribute("name")!,
-            "x-props": this.props ?? "{}",
-            "x-runtimejs-flag": "" + Reflect.get(window, "$runtimeJSFlag"),
-            "x-scope-flag": "" + Reflect.get(window, "$runtimeJSFlag"),
-          },
-        }).then((res) => {
+        const nameAttr = this.getAttribute("name")!;
+        const headers = {
+          "x-component": nameAttr,
+          "x-props": this.props ?? "{}",
+          "x-runtimejs-flag": "" + (window as any).$runtimeJSFlag,
+          "x-scope-flag": "" + (window as any).$scopeFlag,
+        };
+        fetch(location.href, { headers }).then((res) => {
           if (!res.ok) {
-            console.error("Failed to fetch component:", res.status, res.statusText);
-            return;
+            throw new Error("Failed to fetch component '" + nameAttr + "'");
           }
           res.json().then(([html, js]) => {
             this.innerHTML = html;
