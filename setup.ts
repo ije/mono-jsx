b197e0e490fa241396fdebb8a72328ad225ed19b
Je Xia <i@jex.me>
2025-11-19 19:54:46 +0800

    Update setup script

--- setup.ts
+++ setup.ts
@@ -1,9 +1,95 @@
-import { existsSync } from "node:fs";
-import { readFile, writeFile } from "node:fs/promises";
+import { lstat, readFile, writeFile } from "node:fs/promises";
 import { argv } from "node:process";
 
+const serverTSX = `// mono-jsx SSR example
+// Documentation: https://ije.github.io/mono-jsx/docs/ssr
+
+function App(this: FC<{ a: number; b: number }>) {
+  this.init({
+    a: 1,
+    b: 2,
+  })
+
+  this.effect(() => {
+    // effect is called when the component is mounted or when the dependencies(calc.a & calc.b) change
+    console.log("sum", this.a * this.b);
+    return () => {
+      console.log("cleanup");
+    };
+  });
+
+  return (
+    <>
+      <h1>Welcome to mono-jsx!</h1>
+      <div
+        style={{
+          display: "inline-block",
+          padding: "6px 12px",
+          borderRadius: 6,
+          backgroundColor: "#f4f4f6",
+          fontFamily: "monospace",
+          ":hover": {
+            backgroundColor: "#eeeeef",
+          },
+          "& input": {
+            fieldSizing: "content",
+          },
+        }}
+      >
+        <input type="number" $value={this.a} />
+        {" * "}
+        <input type="number" $value={this.b} />
+        {" = "}
+        {this.computed(() => this.a * this.b)}
+      </div>
+    </>
+  );
+}
+
+export default {
+  fetch: (request: Request) => (
+    <html request={request}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Building user interfaces." />
+        <meta name="keywords" content="ssr,jsx" />
+        <title>Welcome to mono-jsx!</title>
+      </head>
+      <body>
+        <App />
+      </body>
+    </html>
+  ),
+};
+`;
+
+async function install() {
+  if (globalThis.Deno) {
+    await (new Deno.Command("deno", {
+      args: ["install", "npm:mono-jsx"],
+    })).spawn().status;
+  } else {
+    await import("node:child_process").then(module => {
+      const result = module.spawnSync("bun", ["i", "mono-jsx"]);
+      if (result.error) {
+        if ((result.error as any).code === "ENOENT") {
+          module.spawnSync("npm", ["install", "mono-jsx"]);
+        } else {
+          throw result.error;
+        }
+      }
+    });
+  }
+}
+
 export async function setup() {
-  if (globalThis.Deno && existsSync("deno.jsonc")) {
+  const csr = argv.includes("--csr");
+  if (!csr && !await exists("server.tsx")) {
+    await writeFile("server.tsx", serverTSX);
+  }
+  if (globalThis.Deno && await exists("deno.jsonc")) {
+    await install();
     console.log("Please add the following options to your deno.jsonc file:");
     console.log(
       [
@@ -27,7 +113,7 @@ export async function setup() {
   } catch {
     // ignore
   }
-  const jsxImportSource = argv.includes("--dom") ? "mono-jsx/dom" : "mono-jsx";
+  const jsxImportSource = csr ? "mono-jsx/dom" : "mono-jsx";
   const compilerOptions = tsConfig.compilerOptions ?? (tsConfig.compilerOptions = {});
   if (compilerOptions.jsx === "react-jsx" && compilerOptions.jsxImportSource === jsxImportSource) {
     console.log("%cmono-jsx already setup.", "color:grey");
@@ -43,5 +129,14 @@ export async function setup() {
   compilerOptions.jsx = "react-jsx";
   compilerOptions.jsxImportSource = jsxImportSource;
   await writeFile(tsConfigFilename, JSON.stringify(tsConfig, null, 2));
+  await install();
   console.log("✅ mono-jsx setup complete.");
+}
+
+async function exists(path: string) {
+  try {
+    return await lstat(path);
+  } catch {
+    return false;
+  }
 }
