3c3993a4916c254bbd7308c2fb96e4053c96810d
Je Xia <i@jex.me>
2026-03-16 17:25:25 +0800

    Fix `formslot` element glitch

--- runtime/form.ts
+++ runtime/form.ts
@@ -32,12 +32,9 @@ customElements.define(
   },
 );
 
-window.$onrfs = async (evt) => {
-  evt.preventDefault();
-  const formEl = evt.target as HTMLFormElement;
-  formEl.querySelectorAll("m-formslot").forEach(el => {
-    el.innerHTML = "";
-  });
+window.$onrfs = async (e) => {
+  e.preventDefault();
+  const formEl = e.target as HTMLFormElement;
   if (!formEl.checkValidity()) {
     return;
   }
@@ -52,56 +49,61 @@ window.$onrfs = async (evt) => {
     headers: { "x-route-form": "true", "x-flags": $FLAGS },
     body: data,
   });
-  const [html, js] = await res.json();
-  const tpl = document.createElement("template");
-  const formslots = new Map<HTMLElement, HTMLElement>();
-  tpl.innerHTML = html;
-  for (const inputEl of inputEls) {
-    inputEl.disabled = inputEl._disabled!;
-    delete inputEl._disabled;
-  }
-  for (const child of tpl.content.childNodes) {
-    if (child.nodeType === 1) {
-      const el = child as HTMLElement;
-      const slot = getAttr(el, "formslot");
-      const selector = slot ? 'm-formslot[name="' + slot + '"]' : "m-formslot";
-      const formslot = slot
-        ? formEl.querySelector(selector) ?? document.querySelector(selector)
-        : formEl.querySelector(selector);
-      if (formslot) {
-        formslot.innerHTML = "";
-        formslots.set(el, formslot as HTMLElement);
-        continue;
+  if (res.ok) {
+    const [html, js] = await res.json();
+    const tpl = document.createElement("template");
+    const formslots = new Map<HTMLElement, HTMLElement>();
+    formEl.querySelectorAll("m-formslot").forEach(el => {
+      el.innerHTML = "";
+    });
+    tpl.innerHTML = html;
+    for (const inputEl of inputEls) {
+      inputEl.disabled = inputEl._disabled!;
+      delete inputEl._disabled;
+    }
+    for (const child of tpl.content.childNodes) {
+      if (child.nodeType === 1) {
+        const el = child as HTMLElement;
+        const slot = getAttr(el, "formslot");
+        const selector = slot ? 'm-formslot[name="' + slot + '"]' : "m-formslot";
+        const formslot = slot
+          ? formEl.querySelector(selector) ?? document.querySelector(selector)
+          : formEl.querySelector(selector);
+        if (formslot) {
+          formslot.innerHTML = "";
+          formslots.set(el, formslot as HTMLElement);
+          continue;
+        }
+      }
+      appendChild(formEl, child);
+    }
+    for (const [child, formslot] of formslots) {
+      const updateFid = getAttr(formslot, "onupdate");
+      const scope = getAttr(formslot, "scope");
+      switch (getAttr(formslot, "mode")) {
+        case "insertbefore":
+          formslot.before(child);
+          break;
+        case "insertafter":
+          formslot.after(child);
+          break;
+        default:
+          appendChild(formslot, child);
+      }
+      if (updateFid) {
+        $fmap.get(Number(updateFid))?.call(
+          $signals?.(Number(scope)) ?? formslot,
+          { type: "update", target: formslot },
+        );
       }
     }
-    appendChild(formEl, child);
-  }
-  for (const [child, formslot] of formslots) {
-    const updateFid = getAttr(formslot, "onupdate");
-    const scope = getAttr(formslot, "scope");
-    switch (getAttr(formslot, "mode")) {
-      case "insertbefore":
-        formslot.before(child);
-        break;
-      case "insertafter":
-        formslot.after(child);
-        break;
-      default:
-        appendChild(formslot, child);
+    setTimeout(() => {
+      if (!inputEls.some(el => !el.validity.valid)) {
+        formEl.reset();
+      }
+    }, 0);
+    if (js) {
+      appendChild(document.body, document.createElement("script")).textContent = js + ";document.currentScript.remove();";
     }
-    if (updateFid) {
-      $fmap.get(Number(updateFid))?.call(
-        $signals?.(Number(scope)) ?? formslot,
-        { type: "update", target: formslot },
-      );
-    }
-  }
-  setTimeout(() => {
-    if (!inputEls.some(el => !el.validity.valid)) {
-      formEl.reset();
-    }
-  }, 0);
-  if (js) {
-    appendChild(document.body, document.createElement("script")).textContent = js;
   }
 };
